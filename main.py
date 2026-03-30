3b61f0ddb02ecc3b8612940e1b0b1e363cd6d9a4
PrantikM <145676123+PrantikM@users.noreply.github.com>
2026-03-30 22:14:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
 
 app = FastAPI()
+
+class post(BaseModel):
+  Title : str
+  content : str
+  published : bool = True
+  rating : Optional[int] = None
+
+
+my_posts = [{"title" : "title of post 1", "content" : "content of post 1", "id" : 1},
+{"title" : "favourite foods", "content" : "i like pizza" , "id" : 2}] 
+
+def findpost(id):
+  for p in my_posts:
+    if p["id"] == id:
+      return p
 
 @app.get("/")
 async def root():
@@ -10,9 +29,17 @@ async def root():
 
 @app.get("/posts")
 async def posts():
-  return{"data" : "these are your posts"}
+  return{"data" : my_posts}
 
-@app.post("/createposts")
-def create_posts(payload :dict = Body(...)):
-  print(payload)
-  return {"new_post" : f"title : {payload['Title']} content : {payload['content']}"}
+@app.post("/posts")
+async def create_posts(new_post :post):
+  post_dict = new_post.dict()
+  post_dict["id"] = randrange(0,10000000)
+  my_posts.append(post_dict)
+  return {"data" : post_dict}
+
+
+@app.get("/posts/{id}")
+async def get_post(id : int):
+  post = findpost(id)
+  return{"post_detail" : post}
