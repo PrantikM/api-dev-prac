6703f5c66510aa4815de2ce3a60ffed3dd6d651d
PrantikM <145676123+PrantikM@users.noreply.github.com>
2026-04-07 10:56:44 +0530

    Update main.py

--- apps/main.py
+++ apps/main.py
@@ -1,14 +1,6 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
+from fastapi import FastAPI, Response
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 
@@ -16,33 +8,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
-
-
-
-while True:
-  try:
-    conn = psycopg2.connect(host='localhost', database = 'api-dev-prac',user ='postgres',
-    password = 'prantik0208', cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("database connection successful")
-    break
-  except Exception as error:
-    print("error while connecting to database", error)  
-    time.sleep(2)
-
-
-my_posts = [{"title" : "title of post 1", "content" : "content of post 1", "id" : 1},
-{"title" : "favourite foods", "content" : "i like pizza" , "id" : 2}] 
-
-def findpost(id):
-  for p in my_posts:
-    if p["id"] == id:
-      return p
-
-def find_index_post(id):
-  for i, p in enumerate(my_posts):
-    if p["id"] == id:
-      return i
 
 
 app.include_router(post.router)
